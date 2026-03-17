12988a7c20ed89733e1082b6e0d8263068238356
Vitor Souza <vitor_martins_@hotmail.com>
2026-03-17 15:52:07 -0300

    correção do código

--- rede_social.py
+++ rede_social.py
@@ -25,7 +25,3 @@ link = engenho(rede_social, "Vitor", "Recrutador")
 print(link)  
 
 
-#O problema que eu busquei trabalhar é o nivel de conexão entre pessoas dentro de uma rede social voltada ao mercado de trabalho.
-# A ideia principal é encontrar pelo nivel de conexão um candidato a uma pessoa que seja recrutadora. 
-# O algoritmo utilizado foi o Breadth-First Search (BFS), que vai buscar pelo nivel de conexão entre os amigos do canditado, expandindo a busca para cada amigo de amigo, até chegar ao recrutador. 
-# Esse algoritmo foi escolhido por ser mais eficiente para encontrar os caminhos mais curtos dos grafos até chegar no destino final, que no caso do problema é o recrutador.
